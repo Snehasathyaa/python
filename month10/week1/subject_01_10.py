60c0e87f203a29cc5b8b3966e8489a3f5cf4e728
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2024-10-04 07:23:54 +0100

    Add files via upload

---
+++ month10/week1/subject_01_10.py
@@ -0,0 +1,9 @@
+print("mark of 5 subjects")
+a=float(input("enter mark a: "))
+b=float(input("enter mark b: "))
+c=float(input("enter mark c: "))
+d=float(input("enter mark d: "))
+e=float(input("enter mark e: "))
+tot=a+b+c+d+e
+print("total mark = ",tot)
+print("total percentage = ",(tot/50)*100 )
