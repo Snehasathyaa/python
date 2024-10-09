bd32a83a322fa486f3db0aa1e496677a72532bcc
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2024-10-09 06:37:48 +0100

    Update swapList_09_10.py

--- month10/week 2/swapList_09_10.py
+++ month10/week 2/swapList_09_10.py
@@ -2,6 +2,6 @@ List=input("Enter a list item ")
 L=[]
 for n in List:
     L.append(n)
-print("Before swaping List = ",L)
+print("Before swaping List\n",L)
 L[0],L[-1]=L[-1],L[0]
-print("After swaping List = ",L)
+print("After swaping List\n",L)
