91fab087f64ebe363b5b74ce1744518aded35126
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2024-10-18 11:45:17 +0100

    Delete month10/week 3/count_16_10.py

--- month10/week 3/count_16_10.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter string: ")
-words=a.split()
-word_count={}
-for word in words:
-    word_count[word]= words.count(word)
-for word.count in word_count.items():
-    print(f"'{word}':{count}")
-print(a)
-
