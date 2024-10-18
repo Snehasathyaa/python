83dabed39b0e367049293e343608629c6357b289
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2024-10-18 11:42:26 +0100

    Add files via upload

---
+++ month10/week 3/count_16_10.py
@@ -0,0 +1,9 @@
+a=input("Enter string: ")
+words=a.split()
+word_count={}
+for word in words:
+    word_count[word]= words.count(word)
+for word.count in word_count.items():
+    print(f"'{word}':{count}")
+print(a)
+
