ad8391b8428b7b837c03bfb0823576c4856e4e58
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2024-11-01 10:15:29 +0000

    Update pyramid_30_10.py

--- month10/week 4/pyramid_30_10.py
+++ month10/week 4/pyramid_30_10.py
@@ -1,7 +1,12 @@
 num = 5
 
 for i in range(1, num + 1):
-    print("* " * i)
+    for j in range(i):
+        
+        print("* " ,end=' ')
+    print()
 
 for i in range(num - 1, 0, -1):
-    print("* " * i)
+    for j in range(i):
+        print("* " ,end=' ')
+    print()
